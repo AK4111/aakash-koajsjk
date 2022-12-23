d7fd3af4b63edb168f3428883b8c7b5274ba2398
Karan Adhikari <doc.adhikari@gmail.com>
2022-12-23 21:19:03 +0530

    Update mirror_leech.py

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -1,4 +1,5 @@
 from base64 import b64encode
+from requests import get as rget
 from pyrogram import enums
 from re import match as re_match, search as re_search, split as re_split
 from time import sleep, time
