3de76b8664a7baaf8138a2471ba57dcca2d0a8d2
Toxy Tech <105636592+mirrordcc@users.noreply.github.com>
2022-12-24 15:14:32 +0530

    Multi fsub line

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -166,7 +166,7 @@ def forcesub(bot, message, tag):
         btn = ButtonMaker()
         for key, value in join_button.items():
             btn.buildbutton(key, value)
-        msg = f'💡 {tag},\nYou have to join our channel(s)!\n🔻 Join And Try Again!'
+        msg = f'💡 {tag},\nYou have to join our channel(s) In Order To Use Bots!\n🔻 Join And Try Again!'
         reply_message = sendMessage(msg, bot, message, btn.build_menu(1))
         return reply_message
 
